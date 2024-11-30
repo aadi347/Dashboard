11c734c3f9ac9ed2f0b192c7a30de4dba94181b9
Aditya Kumar <aadi678a@gmail.com>
2024-11-30 10:06:58 +0530

    first commit on Dasboard App

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// write your code here 
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up and listening on port ${port}`);
+});
