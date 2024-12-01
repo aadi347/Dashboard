0b33a7b7244014c36bed26f457454feb3d9c275c
Aditya Kumar <aadi678a@gmail.com>
2024-12-01 20:44:46 +0530

    database is connected and working fine

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,23 @@ import { PropertyRegistration } from './models/FlatRegistrationModel.js';
 const app = express();
 const port = 3000;
 
+// middleware
+
+app.use(cors({
+    origin: "http://localhost:5173",  
+    credentials: true                
+  }));
+app.use(express.json());
+
+const mongoDbUrl = 'mongodb+srv://aadi678a:UDW84zwTIgVJaTTs@dashboard.epgi5.mongodb.net/?retryWrites=true&w=majority&appName=Dashboard'
+mongoose
+.connect(mongoDbUrl)
+.then(() => {
+    console.log("Cluster is connected");
+    app.listen(port, () => {
+        console.log(`Server is up and listening on port ${port}`);
+    });
+})
+.catch((error) => console.log(error));
 
 
-
-
-app.listen(port, () => {
-    console.log(`Server is up and listening on port ${port}`);
-});
